79721a5036b3281721f0ac3dd4e859c6a6cb0ab9
deshudiosh <deshudiosh@gmail.com>
2018-02-26 19:47:49 +0100

    xxx

--- py_sa_bot.py
+++ py_sa_bot.py
@@ -31,29 +31,44 @@ def vote():
         popup_close = driver.find_element_by_id("newsletter_close")
         popup_close.click()
 
-        publiczne = driver.find_elements_by_xpath("//span[contains(text(), 'PUBLICZNE')]")
-        publiczne = publiczne[0]
-        print("Znalazłem PUBLICZNE")
+        # publiczne = driver.find_elements_by_xpath("//span[contains(text(), 'PUBLICZNE')]")
+        # publiczne = publiczne[0]
+        # print("Znalazłem PUBLICZNE")
+        publiczne = WebDriverWait(driver, 45).until(
+            EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'PUBLICZNE')]"))
+        )
 
-        if publiczne:
+        # if publiczne:
             # find "PUBLICZNE" button by query selector, since no class and no id on the button
-            driver.execute_script("document.querySelector('[data-id=\"question/83\"]').click();")
+        driver.execute_script("document.querySelector('[data-id=\"question/83\"]').click();")
 
-            try:
-                radio = WebDriverWait(driver, 60).until(
-                    EC.presence_of_element_located((By.ID, "r443"))
-                )
-                print("Znalazłem RADIO")
 
-                driver.execute_script("document.getElementById('r443').click();")
+        try:
+            poll_numbers = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "poll-numbers"))
+            )
+            if poll_numbers:
+                print("znalazłem poll-numbers")
+                sleep(3)
+                driver.quit()
+        except:
+            print("nie znalzałem poll-numbers")
 
-                glosuj = driver.find_element_by_class_name("sd")
-                print("Znalazlem GLOSUJ")
+        try:
+            radio = WebDriverWait(driver, 30).until(
+                EC.presence_of_element_located((By.ID, "r443"))
+            )
+            print("Znalazłem RADIO")
 
-                driver.execute_script("document.getElementsByClassName('sd')[0].click();")
+            driver.execute_script("document.getElementById('r443').click();")
 
-            except:
-                print("--> nie znalazłem RADIO/GLOSUJ")
+            glosuj = driver.find_element_by_class_name("sd")
+            print("Znalazlem GLOSUJ")
+
+            driver.execute_script("document.getElementsByClassName('sd')[0].click();")
+
+        except:
+            print("--> nie znalazłem RADIO/GLOSUJ")
 
     except:
         print("--> nie znalazłem PUBLICZNE")
